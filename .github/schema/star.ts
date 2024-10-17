5cae902eabf95370169199ef3806418607c8ec87
chelgeson1click <chelgeson@1clicklogistics.com>
2024-10-17 10:41:16 -0700

    graphql tests

---
+++ .github/schema/star.ts
@@ -0,0 +1,9 @@
+const s = `#graphql
+query($org: String!, $number: Int!) {
+    organization {
+        announcement
+    }
+}
+`;
+
+const s = 4; 
---
+++ graphql.config.ts
@@ -0,0 +1,9 @@
+import { IGraphQLConfig } from 'graphql-config';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+export default {
+  schema: 'githubSchema.graphql',
+  documents: './*.{graphql,ts}',
+} satisfies IGraphQLConfig;
