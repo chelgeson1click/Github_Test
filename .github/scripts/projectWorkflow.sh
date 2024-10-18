7c95a91a2e43b048a41cf1d5ced82df64b5ada9e
chelgeson1click <chelgeson@1clicklogistics.com>
2024-10-18 11:31:13 -0700

    test

---
+++ .github/scripts/projectWorkflow.sh
@@ -0,0 +1,13 @@
+runGraphQLQuery() {
+  local graphqlFileName=$1
+  local queryOutput=$2
+
+  gh api graphql -f query="$(cat ./.github/schema/${graphqlFileName}.graphql)" > ${queryOutput}
+}
+
+getProjectVariableID() {
+  local envVarName=$1
+  local fieldVarName=$2
+  local field_id=$(jq ".data.user.projectV2.fields.nodes[] | select(.name==\"${fieldVarName}\") | .id" project_data.json)
+  echo "${envVarName}=${field_id}" >> $GITHUB_ENV
+}
